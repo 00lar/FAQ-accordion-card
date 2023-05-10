c66babc647f8cc7304b274e24a2a5544b6e4df0f
00lartks <eduardani84@gmail.com>
2023-05-10 10:33:11 -0700

    Javascript implementacion

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const RowBtn = document.querySelector("#RowBtn");
+const answer = document.querySelector("#answer");
+const pPregunta = document.querySelector("#pPregunta");
 
-RowBtn.addEventListener(click,Display);
+RowBtn.addEventListener("click", ocultar);
 
-function Display(){}
+function ocultar() {
+  if (answer.classList.contains("Ocultar")) {
+    answer.classList.remove("Ocultar");
+
+    pPregunta.classList.add("Negritas");
+  } else {
+    answer.classList.add("Ocultar");
+    pPregunta.classList.remove("Negritas");
+  }
+}
